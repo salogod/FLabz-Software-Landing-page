1378f1267af42ee72821a98eb725b811dee4308b
salogod <102626192+salogod@users.noreply.github.com>
2023-11-07 20:27:26 -0600

    Add files via upload

--- Landing page FLabz/slider.js
+++ Landing page FLabz/slider.js
@@ -19,4 +19,26 @@ document.addEventListener("DOMContentLoaded", function() {
     prevBtn.addEventListener("click", function() {
         goToSlide(currentSlide - 1);
     });
+
+    // Configuración del intervalo para cambiar los slides automáticamente
+    let slideInterval = setInterval(function() {
+        goToSlide(currentSlide + 1);
+    }, 5000);
+
+    // Opcional: Limpia el intervalo cuando el usuario interactúa con los botones
+    nextBtn.addEventListener("click", function() {
+        clearInterval(slideInterval);
+        // Reiniciar el intervalo
+        slideInterval = setInterval(function() {
+            goToSlide(currentSlide + 1);
+        }, 3000);
+    });
+
+    prevBtn.addEventListener("click", function() {
+        clearInterval(slideInterval);
+        // Reiniciar el intervalo
+        slideInterval = setInterval(function() {
+            goToSlide(currentSlide - 1);
+        }, 5000);
+    });
 });
